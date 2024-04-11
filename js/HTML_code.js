b9b7f4e1a3be53fcd91a47e2da27fd9d8c1fcf28
Ever VC <ever.vasquezc27@gmail.com>
2024-04-11 15:13:16 -0600

    Se creó un archivo para todo el HTML que se inserta desde el JS

---
+++ js/HTML_code.js
@@ -0,0 +1,146 @@
+export const drawPokemonCard = (pokemon) => {
+    let card = '';//Contendrá la estructura de la card
+    //Asigna los datos correspondientes de la card
+    card += `
+        <div class="card blur-in" data-card-${pokemon.id}>
+            <p class="hp">
+                <span>HP #${pokemon.id.toString().padStart(3,0)}</span>
+            </p>
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
+            <h2 class="poke-name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+            <div class="types" data-type-${pokemon.id}>
+
+            </div>
+            <div class="stats">
+                <div>
+                    <h3>${pokemon.stats[1].base_stat}</h3>
+                    <p>Attack</p>
+                </div>
+                <div>
+                    <h3>${pokemon.stats[2].base_stat}</h3>
+                    <p>Defense</p>
+                </div>
+                <div>
+                    <h3>${pokemon.stats[5].base_stat}</h3>
+                    <p>Speed</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    return card;//Retorna la estructura de la card
+}
+
+export const drawLoadContainer = () => {
+    let divLoadContainer = '';//Variable que contiene el contenido a mostrar
+    divLoadContainer += `
+        <section class="load-container blur-in">
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <p>Loading...</p>
+        </section>
+    `;
+    return divLoadContainer;
+}
+
+export const drawCardModal = (pokemon) => {
+    let cardModal = '';//Contiene toda la estructura del modal
+    //Le asigna la estructura del modal incluyendo los valores de cada propiedad a mostrar
+    cardModal += `
+    <article class="modal_container blur-in-expand" data-modalCard-${pokemon.id}>
+        <div class="modal-id">
+            <p class="hp">
+                <span>HP #${pokemon.id.toString().padStart(3,0)}</span>
+            </p>
+        </div>
+        <div class="modal-main">
+        <img src="${pokemon.sprites.versions['generation-v']['black-white'].animated.front_default}" alt="">
+            <h2 class="poke-name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+            <div class="types" data-modalTypes-${pokemon.id}>
+            </div>
+        </div>
+        <div class="modal-about">
+            <div class="nav">
+                <a href="#" data-about>About</a>
+                <a href="#" data-stats>Base Stats</a>
+                <a href="#" data-moves>Moves</a>
+                <div class="animation start-home"></div>
+            </div>
+            <div class="information" data-modalInfo>
+                
+            </div>
+        </div>
+        <a class="modal_close" data-modalClose>CLOSE</a>
+    </article>
+    `;
+    return cardModal;
+}
+
+export const showInformation = (pokemon, divInformation, typeInfo) => {
+    let information = '';//Almacena la estructura que contendrá el apartado
+
+    if (typeInfo == "about") {//verifica si se dió click en el botón "about"
+        let height = parseInt(pokemon.height) / 10;//La altura se divide entre 10 para convertir de decímetros a metros
+        let weight = parseInt(pokemon.weight) / 10; //El peso se divide entre 10 para convertir de hetogramos a kilogramos          
+        //Se le asigna la estructura e información correspondiente
+        information += `
+            <p>Species:<span>${pokemon.species.name[0].toUpperCase() + pokemon.species.name.slice(1)}</span></p>
+            <p>Height:       <span>${height} m</span></p>
+            <p>Weight:       <span>${weight} kg</span></p>
+            <p>Abilities:<span class="abilities" data-abilities></span></p>
+            <p>Base Experience:<span class="base-experience">${pokemon.experience} pst.</span></p>
+        `;
+        divInformation.innerHTML = information;//Se inserta la estructura en el contenedor
+
+        const spanAbilities = document.querySelector("[data-abilities]");//Alccede al Span que contendrá las habilidades
+        //Recorre cada una de las habilidades de pokémon y se agregan al texto del span
+        pokemon.abilities.forEach((ability) => {
+            spanAbilities.textContent += ability.ability.name + " ";
+        })
+    } else if (typeInfo == "stats") {
+        //Si el botón al que se dió click es de "stats", se agregan cada una de las estadisticas principales al contenedor
+        information += `
+            <div class="info-data">
+                <div class="data-name">
+                    <p>HP:</p>
+                    <p>Attack:</p>
+                    <p>Defence:</p>
+                    <p>Sp. Attk:</p>
+                    <p>Sp. Def:</p>
+                    <p>Speed:</p>
+                </div>
+                <div class="data-value">
+                    <div class="bar"><div style="width: ${(pokemon.stats[0].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[0].base_stat}</div></div>
+                    <div class="bar"><div style="width: ${(pokemon.stats[1].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[1].base_stat}</div></div>
+                    <div class="bar"><div style="width: ${(pokemon.stats[2].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[2].base_stat}</div></div>
+                    <div class="bar"><div style="width: ${(pokemon.stats[3].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[3].base_stat}</div></div>
+                    <div class="bar"><div style="width: ${(pokemon.stats[4].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[4].base_stat}</div></div>
+                    <div class="bar"><div style="width: ${(pokemon.stats[5].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[5].base_stat}</div></div>
+                </div>
+            </div>
+        `;
+        //------ IMPORTANTE: las estadísticas se dividen entre 200 y se multiplican por 100, ya que algunas
+        //exceden el 100% y se indica que el valor máximo es de 200 para el ancho del contenedor
+        divInformation.innerHTML = information;//Se inserta la estructura
+        
+    } else if (typeInfo == "moves") {
+        //En caso de ser el botón de "moves" al que se le dió click, se asigna su estructura correspondiente
+        information += `
+            <div class="info-moves">
+            </div>
+        `;
+        divInformation.innerHTML = information;
+        const movesInfo = document.querySelector(".info-moves");//Accede al div que contendrá los movimientos
+        for (let i = 0; i < pokemon.moves.length; i++) {//Recorre el arreglo de movimientos
+            if (i <= 20) {
+                const pMove = document.createElement("p");//Crea un elemento "p" por cada movimiento
+                pMove.textContent = pokemon.moves[i].move.name;//Le asigna el nombre del movimiento
+                movesInfo.appendChild(pMove);//Agrega el elemento "p" al contenedor
+            } else {
+                break;//Cuando llegue a los 20 movimientos (porque algunos pokémons tienen más), saldrá del bucle
+                //para no mostrar tantos
+            }
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Se importan las librerias
 import { Pokemon } from "./PokemonClass.js";
+import { drawPokemonCard, drawLoadContainer, drawCardModal, showInformation } from "./HTML_code.js";
 import { typeColor } from "./colors.js";
 
 //Implementando el patron módulo
@@ -109,16 +110,8 @@ const Pokedex = (() => {
     const _waitingCardsLoad = () => {
         _removeAllCards();//Elimina todos los elementos que existan en el "main"
         const cards = document.querySelector("[data-cards]");//Accede al elemento "main"
-        let dinvLoadContainer = '';//Variable que contiene el contenido a mostrar
-        dinvLoadContainer += `
-            <section class="load-container blur-in">
-                <div class="ring"></div>
-                <div class="ring"></div>
-                <div class="ring"></div>
-                <p>Loading...</p>
-            </section>
-        `;
-        cards.innerHTML = dinvLoadContainer;//Se le inserta el contenido al elemnto "main"
+        
+        cards.innerHTML = drawLoadContainer();//Se le inserta el contenido al elemnto "main"
     }
 
     const _addEventsToCards = () => {
@@ -158,36 +151,7 @@ const Pokedex = (() => {
             data.base_experience
         );
 
-        let cardModal = '';//Contiene toda la estructura del modal
-        //Le asigna la estructura del modal incluyendo los valores de cada propiedad a mostrar
-        cardModal += `
-        <article class="modal_container blur-in-expand" data-modalCard-${pokemon.id}>
-            <div class="modal-id">
-                <p class="hp">
-                    <span>HP #${pokemon.id.toString().padStart(3,0)}</span>
-                </p>
-            </div>
-            <div class="modal-main">
-            <img src="${pokemon.sprites.versions['generation-v']['black-white'].animated.front_default}" alt="">
-                <h2 class="poke-name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
-                <div class="types" data-modalTypes-${pokemon.id}>
-                </div>
-            </div>
-            <div class="modal-about">
-                <div class="nav">
-                    <a href="#" data-about>About</a>
-                    <a href="#" data-stats>Base Stats</a>
-                    <a href="#" data-moves>Moves</a>
-                    <div class="animation start-home"></div>
-                </div>
-                <div class="information" data-modalInfo>
-                    
-                </div>
-            </div>
-            <a class="modal_close" data-modalClose>CLOSE</a>
-        </article>
-        `;
-        modalSection.innerHTML = cardModal;//Se le inserta el contenido al elemnto "modalSection"
+        modalSection.innerHTML = drawCardModal(pokemon);//Se le inserta el contenido al elemnto "modalSection"
         modalSection.classList.add("modal--show");//Le agrega la clase "modal--show", la cual le permite mostrarlo a través del css
 
         const divTypes = document.querySelector(`[data-modalTypes-${pokemon.id}]`);//Accede al div que contiene los "elementos" del pokémon
@@ -272,71 +236,7 @@ const Pokedex = (() => {
         //también recibe el titpo de información que se desea mostrar (depende del botón al que se le haya dado clik)
         const divInformation = document.querySelector("[data-modalInfo]");//Accede al apartado que muestra la información del pokémon en el modal
         divInformation.classList.add("blur-in");//le asigna la clase que permite hacer el efecto blur
-        let information = '';//Almacena la estructura que contendrá el apartado
-
-        if (typeInfo == "about") {//verifica si se dió click en el botón "about"
-            let height = parseInt(pokemon.height) / 10;//La altura se divide entre 10 para convertir de decímetros a metros
-            let weight = parseInt(pokemon.weight) / 10; //El peso se divide entre 10 para convertir de hetogramos a kilogramos          
-            //Se le asigna la estructura e información correspondiente
-            information += `
-                <p>Species:<span>${pokemon.species.name[0].toUpperCase() + pokemon.species.name.slice(1)}</span></p>
-                <p>Height:       <span>${height} m</span></p>
-                <p>Weight:       <span>${weight} kg</span></p>
-                <p>Abilities:<span class="abilities" data-abilities></span></p>
-                <p>Base Experience:<span class="base-experience">${pokemon.experience} pst.</span></p>
-            `;
-            divInformation.innerHTML = information;//Se inserta la estructura en el contenedor
-
-            const spanAbilities = document.querySelector("[data-abilities]");//Alccede al Span que contendrá las habilidades
-            //Recorre cada una de las habilidades de pokémon y se agregan al texto del span
-            pokemon.abilities.forEach((ability) => {
-                spanAbilities.textContent += ability.ability.name + " ";
-            })
-        } else if (typeInfo == "stats") {
-            //Si el botón al que se dió click es de "stats", se agregan cada una de las estadisticas principales al contenedor
-            information += `
-                <div class="info-data">
-                    <div class="data-name">
-                        <p>HP:</p>
-                        <p>Attack:</p>
-                        <p>Defence:</p>
-                        <p>Sp. Attk:</p>
-                        <p>Sp. Def:</p>
-                        <p>Speed:</p>
-                    </div>
-                    <div class="data-value">
-                        <div class="bar"><div style="width: ${(pokemon.stats[0].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[0].base_stat}</div></div>
-                        <div class="bar"><div style="width: ${(pokemon.stats[1].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[1].base_stat}</div></div>
-                        <div class="bar"><div style="width: ${(pokemon.stats[2].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[2].base_stat}</div></div>
-                        <div class="bar"><div style="width: ${(pokemon.stats[3].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[3].base_stat}</div></div>
-                        <div class="bar"><div style="width: ${(pokemon.stats[4].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[4].base_stat}</div></div>
-                        <div class="bar"><div style="width: ${(pokemon.stats[5].base_stat)/200 * 100}%" class="hp-value">${pokemon.stats[5].base_stat}</div></div>
-                    </div>
-                </div>
-            `;
-            //------ IMPORTANTE: las estadísticas se dividen entre 200 y se multiplican por 100, ya que algunas
-            //exceden el 100% y se indica que el valor máximo es de 200 para el ancho del contenedor
-            divInformation.innerHTML = information;//Se inserta la estructura
-            
-        } else if (typeInfo == "moves") {
-            //En caso de ser el botón de "moves" al que se le dió click, se asigna su estructura correspondiente
-            information += `
-                <div class="info-moves">
-                </div>
-            `;
-            divInformation.innerHTML = information;
-            const movesInfo = document.querySelector(".info-moves");//Accede al div que contendrá los movimientos
-            for (let i = 0; i < pokemon.moves.length; i++) {//Recorre el arreglo de movimientos
-                if (i <= 20) {
-                    const pMove = document.createElement("p");//Crea un elemento "p" por cada movimiento
-                    pMove.textContent = pokemon.moves[i].move.name;//Le asigna el nombre del movimiento
-                    movesInfo.appendChild(pMove);//Agrega el elemento "p" al contenedor
-                } else {
-                    break;//Cuando llegue a los 20 movimientos (porque algunos pokémons tienen más), saldrá del bucle
-                    //para no mostrar tantos
-                }
-            }
-        }
+        showInformation(pokemon, divInformation, typeInfo);//Hace el llamado a la función que muestra la información del pokémon
     }
 
     const _showPokemonsLeaked = async (type) => {
@@ -406,36 +306,10 @@ const Pokedex = (() => {
                 data.abilities,
                 data.base_experience
             );
-    
-            let card = '';//Contendrá la estructura de la card
-            //Asigna los datos correspondientes de la card
-            card += `
-                <div class="card blur-in" data-card-${pokemon.id}>
-                    <p class="hp">
-                        <span>HP #${pokemon.id.toString().padStart(3,0)}</span>
-                    </p>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
-                    <h2 class="poke-name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
-                    <div class="types" data-type-${pokemon.id}>
-    
-                    </div>
-                    <div class="stats">
-                        <div>
-                            <h3>${pokemon.stats[1].base_stat}</h3>
-                            <p>Attack</p>
-                        </div>
-                        <div>
-                            <h3>${pokemon.stats[2].base_stat}</h3>
-                            <p>Defense</p>
-                        </div>
-                        <div>
-                            <h3>${pokemon.stats[5].base_stat}</h3>
-                            <p>Speed</p>
-                        </div>
-                    </div>
-                </div>
-            `;
-            _cardContainer.innerHTML = card;//Inserta la estructura en el contenedor
+        
+
+            _cardContainer.innerHTML = drawPokemonCard(pokemon);//Llama a la función que crea la card y le pasa el objeto pokémon
+            
             const divTypes = document.querySelector(`[data-type-${pokemon.id}]`);//Accede al div que contendrá los tipos del pokémon
             const cardTheme = document.querySelector(`[data-card-${pokemon.id}]`);//Accede a la card creada
             _appednTypes(pokemon.types, divTypes);//Hace el llamado a la función que agregará cada uno de los tipos a la card, recibe como parámetros el 
